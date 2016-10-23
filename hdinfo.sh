13adb72984e45d74f596abc70e53bb5b1408bc41
honsys <davidb.hon@gmail.com>
2016-10-23 12:30:32 -0400

    dard drive info script relies on hdparm, flist,and lsblk

--- hdinfo.sh
+++ hdinfo.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+echo 'using lsblk, df, fdisk, and hdparm to provide disk and filt-system info'
+echo 'remember, fdisk and hdparm need root ...'
 lsblk
 echo ----------------------------------------------
-fdisk -l | grep /dev  | sort -u
-echo ----------------------------------------------
 df -hT
+echo ----------------------------------------------
+fdisk -l | grep /dev  | sort -u
 echo ----------------------------------------------
 hdparm -i /dev/sda /dev/sdb | egrep -i '/dev|model'
